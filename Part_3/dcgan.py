6227dfee9afcdc6c8d6491c95841f89f81ce1e5d
James <james@localhost>
2023-09-09 09:42:34 +1000

    created models

--- Part_3/dcgan.py
+++ Part_3/dcgan.py
@@ -17,47 +17,118 @@ from IPython.display import HTML
 
 # Set random seed for reproducibility
 manualSeed = 999
-#manualSeed = random.randint(1, 10000) # use if you want new results
 print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 torch.use_deterministic_algorithms(True) # Needed for reproducible results
 
-dataroot = "/home/james/Dev/COMP3710/COMP3710-Demo-2/Part_3/data"
+dataroot = "data"
 
 workers = 2
 batch_size = 128
 image_size = 64
-nc = 3
-nz = 100
-ngf = 64
-ndf = 64
-num_epochs = 5
-lr = 0.0002
-beta1 = 0.5
-ngpu = 1
+channels = 3
+latent_size = 100
+epochs = 5
+learning_rate = 0.0002
 
-dataset = datasets.CelebA(
-    root=dataroot, 
-    download=True, 
-    transform=transforms.Compose(
-        [
-            transforms.Resize(image_size),
-            transforms.CenterCrop(image_size),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ]
-    )
-)
+ngpu=1
+
+# Size of feature maps in generator
+ngf = 64
+# Size of feature maps in discriminator
+ndf = 64
+# Beta1 hyperparameter for Adam optimizers
+beta1 = 0.5
+
+
+dataset = datasets.ImageFolder(root=dataroot,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
 
 # Create the dataloader
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                         shuffle=True, num_workers=workers)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
 
+# Decide which device we want to run on
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
-real_batch = next(iter(dataloader))
-plt.figure(figsize=(8,8))
-plt.axis("off")
-plt.title("Training Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+# Generator class
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=latent_size, out_channels=8*ngf, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(8*ngf),
+            nn.ReLU(True)
+            ,
+            nn.ConvTranspose2d(8*ngf, 4*ngf, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(4*ngf, 2*ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d( 2*ngf, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d( ngf, channels, 4, 2, 1, bias=False),
+            nn.Tanh() # Output value between [-1, 1] to match normalized data
+        )
+
+    def forward(self, input):
+        return self.main(input)
+    
+
+# Discriminator class
+"""
+Use strided convolution instead of pooling to downsample as the network learns its own pooling funciton. 
+LeakyRelU instead of RelU apparently performs better.
+"""
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            nn.Conv2d(in_channels=channels, out_channels=ndf, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(ndf, 2*ndf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(2*ndf),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(2*ndf, 4*ndf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(4*ndf),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(4*ndf, 8*ndf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(8*ndf),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(8*ndf, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+
+# Create the generator
+generator_network = Generator(ngpu).to(device)
+# Create the Discriminator
+discriminator_network = Discriminator(ngpu).to(device)
+
+real_image = 1
+generated_image = 0
+
+# Setup Adam optimizers for both Generator and Discriminator
+optimizerD = optim.Adam(discriminator_network.parameters(), lr=learning_rate, betas=(beta1, 0.999))
+optimizerG = optim.Adam(generator_network.parameters(), lr=learning_rate, betas=(beta1, 0.999))
+
